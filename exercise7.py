5c637edc51860a25e0065f1cb1597a3ed24d0d99
1duck2duck3duck <rcernorizov@mail.ru>
2026-06-26 01:15:01 +0300

    Update exercise7.py
    Целые числа и числа с плавающей точкой
    5.3. Задача: выполнение вычислений с пользовательским вводом

--- exercise7.py
+++ exercise7.py
@@ -36,6 +36,3 @@
 ##cipher_phrase = ((((((phrase.replace("a", "4")).replace("b", "8")).replace("e", "3"))\
 ##                .replace("l", "1")).replace("o", "0")).replace("s", "5")).replace("t", "7")
 ##print(cipher_phrase)
-
-string = "I like to eat eggs and spam."
-print(string.strip())
