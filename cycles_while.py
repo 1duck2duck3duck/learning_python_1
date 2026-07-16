e94a03bdbbef8da011222e471cffe05b663e1639
1duck2duck3duck <rcernorizov@mail.ru>
2026-07-16 11:28:58 +0300

    Create cycles_while.py

---
+++ cycles_while.py
@@ -0,0 +1,29 @@
+### Циклы while
+##n = 1
+##while n < 5:
+##    print(n)
+##    n += 1
+##
+##
+##word = "Python"
+##index = 0
+###Один и тот же код while
+##while index < len(word):
+##    print(word[index])
+##    index += 1
+##
+###И так же через for
+##word = "Python"
+##for letter in word:
+##    print(letter)
+##
+### while нужен для длинного или бесконечного цикла и проверок, а for для пересчета конечного и быстрого
+##amount = float(input("Enter amount:  "))
+##
+##for num_people in range(1, 6):
+##    print(f"{num_people} people: ${amount / num_people:,.2f} each")
+
+
+##for n in range(1,4):
+##    for j in range(4,7):
+##        print(f"n = {n} and j = {j}")
