c89e053763d90e3d88b126c620b2751758f471a3
1duck2duck3duck <rcernorizov@mail.ru>
2026-07-21 21:51:45 +0300

    Create check_character.py

---
+++ check_character.py
@@ -0,0 +1,7 @@
+num_user = float(input("Enter is num:  "))
+if num_user > 0:
+    print("This is positive char")
+elif num_user == 0:
+    print("This is zero")
+else:
+    print("This is negative char")
