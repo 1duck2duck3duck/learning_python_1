fdb434c305d366e68066c36c0a5f599637a71412
1duck2duck3duck <rcernorizov@mail.ru>
2026-07-23 21:40:52 +0300

    Create if_else_8.1.py

---
+++ if_else_8.1.py
@@ -0,0 +1,48 @@
+##word_input = input("enter word:  ")
+##l_word = len(word_input)
+##if l_word == 5:
+##    print("Len word = 5")
+##elif l_word < 5:
+##    print("Len word is low 5")
+##else:
+##    print("Len word is up 5")
+##
+##
+##num_user = int(input("Я загадал число от 1 до 10, отгадай:  "))
+##
+##if num_user == 3:
+##    print("You win")
+##else:
+##    print("You lose")
+##
+
+#Задача: поиск множителей числа
+# factors.ry
+
+##def check_int(user_int, n):
+##    if user_int % n == 0:
+##        print(f"{n} is a factors of {user_int}")
+##
+##user_int = int(input("Enter a positive integer:  "))
+##n = 0
+##while n <= user_int:
+##    n+=1
+##    check_int(user_int, n)
+
+
+##user_input = input("Enter data:  ")
+##while True:
+##    if user_input.lower() == "q":
+##        print("Ending...")
+##        break
+##    else:
+##        user_input = input("Enter data:  ")
+
+
+##n = 0
+##for n in range(50):
+##    if n%3 != 0:
+##        print(n)
+##    else:
+##        continue
+    
